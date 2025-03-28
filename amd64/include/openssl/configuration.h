0a4e77b5a95b94f52bd63819344991c74e289e9c
Steve Dower <steve.dower@python.org>
2025-03-28 13:20:43 +0000

    Adds build for OpenSSL 3.0.16 without uplink. This means that applink.c no longer needs to be linked into the host executable. The file remains for now so that builds don't break, but it will have no effect.

--- amd64/include/openssl/configuration.h
+++ amd64/include/openssl/configuration.h
@@ -103,6 +103,9 @@ extern "C" {
 # ifndef OPENSSL_NO_UNIT_TEST
 #  define OPENSSL_NO_UNIT_TEST
 # endif
+# ifndef OPENSSL_NO_UPLINK
+#  define OPENSSL_NO_UPLINK
+# endif
 # ifndef OPENSSL_NO_WEAK_SSL_CIPHERS
 #  define OPENSSL_NO_WEAK_SSL_CIPHERS
 # endif
