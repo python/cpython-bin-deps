2f788ebecac8d4bc3c7fa982b55a6c6923aa55fb
Zachary Ware <zach@python.org>
2026-05-12 08:46:45 -0500

    Import build of Tcl/Tk 9.0.3

--- amd64/lib/tclConfig.sh
+++ amd64/lib/tclConfig.sh
@@ -9,27 +9,23 @@
 #
 # The information in this file is specific to a single platform.
 
-TCL_DLL_FILE="tcl86t.dll"
+TCL_DLL_FILE="tcl90.dll"
 
 # Tcl's version number.
-TCL_VERSION='8.6'
-TCL_MAJOR_VERSION='8'
-TCL_MINOR_VERSION='6'
-TCL_PATCH_LEVEL='8.6.15'
+TCL_VERSION='9.0'
+TCL_MAJOR_VERSION='9'
+TCL_MINOR_VERSION='0'
+TCL_PATCH_LEVEL='9.0.3'
 
 # C compiler to use for compilation.
 TCL_CC='cl'
 
 # -D flags for use with the C compiler.
-TCL_DEFS='-nologo -c /D_ATL_XP_TARGETING  /DHAVE_CPUID=1 -W3 -wd4311 -wd4312 -wd4090 -wd4146 -wd4311 -wd4312 -FpD:\a\1\b\externals\tcl-core-8.6.15.0\win\Release_AMD64_VC13\tcl_ThreadedDynamic\  -fp:strict -O2 -GS -GL -MD -I"D:\a\1\b\externals\tcl-core-8.6.15.0\win\..\win" -I"D:\a\1\b\externals\tcl-core-8.6.15.0\win\..\generic"  -I"D:\a\1\b\externals\tcl-core-8.6.15.0\win\..\libtommath"  /DMP_PREC=4 /Dinline=__inline /DHAVE_ZLIB=1 /D_CRT_SECURE_NO_DEPRECATE /D_CRT_NONSTDC_NO_DEPRECATE /DMP_FIXED_CUTOFFS /DSTDC_HEADERS /DUSE_NMAKE=1 /DHAVE_STDINT_H=1 /DHAVE_INTTYPES_H=1 /DHAVE_STDBOOL_H=1 /DTCL_THREADS=1 /DUSE_THREAD_ALLOC=1 /DNDEBUG /DTCL_CFG_OPTIMIZED /DTCL_CFG_DO64BIT   /DBUILD_tcl'
-
-# If TCL was built with debugging symbols, generated libraries contain
-# this string at the end of the library name (before the extension).
-TCL_DBGX=t
+TCL_DEFS='-nologo -c   /DHAVE_CPUID=1 -W3 -wd4311 -wd4312 -wd4090 -wd4146 -wd4311 -wd4312 -FpD:\a\1\b\externals\tcl-9.0.3.0\win\Release_AMD64_VC13\tcl_Dynamic\  -fp:strict -O2 -GS -GL -MD -I"D:\a\1\b\externals\tcl-9.0.3.0\win\..\win" -I"D:\a\1\b\externals\tcl-9.0.3.0\win\..\generic"  -I"D:\a\1\b\externals\tcl-9.0.3.0\win\..\libtommath"  /DMP_PREC=4 /Dinline=__inline /D_CRT_SECURE_NO_DEPRECATE /D_CRT_NONSTDC_NO_DEPRECATE /DMP_FIXED_CUTOFFS /DSTDC_HEADERS /DUSE_NMAKE=1 /DHAVE_STDINT_H=1 /DHAVE_INTTYPES_H=1 /DHAVE_STDBOOL_H=1 /DTCL_WITH_EXTERNAL_TOMMATH /DMP_64BIT /DNDEBUG /DTCL_CFG_OPTIMIZED /DTCL_CFG_DO64BIT   /DBUILD_tcl'
 
 # Default flags used in an optimized and debuggable build, respectively.
-TCL_CFLAGS_DEBUG='-nologo -c -W3 -YX -FpD:\a\1\b\externals\tcl-core-8.6.15.0\win\Release_AMD64_VC13\tcl_ThreadedDynamic\ -MDd'
-TCL_CFLAGS_OPTIMIZE='-nologo -c -W3 -YX -FpD:\a\1\b\externals\tcl-core-8.6.15.0\win\Release_AMD64_VC13\tcl_ThreadedDynamic\ -MD'
+TCL_CFLAGS_DEBUG='-nologo -c -W3 -YX -FpD:\a\1\b\externals\tcl-9.0.3.0\win\Release_AMD64_VC13\tcl_Dynamic\ -MDd'
+TCL_CFLAGS_OPTIMIZE='-nologo -c -W3 -YX -FpD:\a\1\b\externals\tcl-9.0.3.0\win\Release_AMD64_VC13\tcl_Dynamic\ -MD'
 
 # Default linker flags used in an optimized and debuggable build, respectively.
 TCL_LDFLAGS_DEBUG='-nologo -machine:AMD64 -debug -debugtype:cv'
@@ -39,13 +35,13 @@ TCL_LDFLAGS_OPTIMIZE='-nologo -machine:AMD64 -release -opt:ref -opt:icf,3'
 TCL_SHARED_BUILD=1
 
 # The name of the Tcl library (may be either a .a file or a shared library):
-TCL_LIB_FILE='tcl86t.lib'
+TCL_LIB_FILE='tcl90.lib'
+
+# The name of a zip containing the /library and /encodings (may be either a .zip file or a shared library):
+TCL_ZIP_FILE='@TCL_ZIP_FILE@'
 
 # Flag to indicate whether shared libraries need export files.
 TCL_NEEDS_EXP_FILE=''
-
-# Deprecated. Same as TCL_UNSHARED_LIB_SUFFIX
-TCL_EXPORT_FILE_SUFFIX='86t.lib'
 
 # Additional libraries to use when linking Tcl.
 TCL_LIBS='kernel32.lib advapi32.lib netapi32.lib user32.lib userenv.lib ws2_32.lib ucrt.lib netapi32.lib user32.lib userenv.lib ws2_32.lib'
@@ -68,13 +64,13 @@ TCL_CFLAGS_WARNING='-W3'
 TCL_EXTRA_CFLAGS='-YX'
 
 # Base command to use for combining object files into a shared library:
-TCL_SHLIB_LD='link -nologo -machine:AMD64  -ltcg -release -opt:ref -opt:icf,3 -nodefaultlib:libucrt.lib -dll'
+TCL_SHLIB_LD='link -nologo -machine:AMD64  -ltcg -release -opt:ref -opt:icf,3 -nodefaultlib:ucrt.lib -dll'
 
 # Base command to use for combining object files into a static library:
 TCL_STLIB_LD='lib -nologo'
 
 # Either '$LIBS' (if dependent libraries should be included when linking
-# shared libraries) or an empty string.  See Tcl's configure.in for more
+# shared libraries) or an empty string.  See Tcl's configure.ac for more
 # explanation.
 TCL_SHLIB_LD_LIBS='kernel32.lib advapi32.lib netapi32.lib user32.lib userenv.lib ws2_32.lib ucrt.lib netapi32.lib user32.lib userenv.lib ws2_32.lib'
 
@@ -104,15 +100,15 @@ TCL_COMPAT_OBJS=''
 TCL_RANLIB=''
 
 # -l flag to pass to the linker to pick up the Tcl library
-TCL_LIB_FLAG='tcl86t.lib'
+TCL_LIB_FLAG='tcl90.lib'
 
 # String to pass to linker to pick up the Tcl library from its
 # build directory.
-TCL_BUILD_LIB_SPEC='D:\a\1\b\externals\tcl-core-8.6.15.0\win\Release_AMD64_VC13\tcl86t.lib'
+TCL_BUILD_LIB_SPEC='D:\a\1\b\externals\tcl-9.0.3.0\win\Release_AMD64_VC13\tcl90.lib'
 
 # String to pass to linker to pick up the Tcl library from its
 # installed directory.
-TCL_LIB_SPEC='D:\a\1\a\amd64\lib\tcl86t.lib'
+TCL_LIB_SPEC='D:\a\1\a\amd64\lib\tcl90.lib'
 
 # String to pass to the compiler so that an extension can
 # find installed Tcl headers.
@@ -129,13 +125,13 @@ TCL_LIB_VERSIONS_OK='nodots'
 # extension, and anything else needed).  May depend on the variables
 # VERSION and SHLIB_SUFFIX.  On most UNIX systems this is
 # ${VERSION}${SHLIB_SUFFIX}.
-TCL_SHARED_LIB_SUFFIX='86t.dll'
+TCL_SHARED_LIB_SUFFIX='90.dll'
 
 # String that can be evaluated to generate the part of an unshared library
 # name that comes after the "libxxx" (includes version number, if any,
 # extension, and anything else needed).  May depend on the variable
 # VERSION.  On most UNIX systems this is ${VERSION}.a.
-TCL_UNSHARED_LIB_SUFFIX='86t.lib'
+TCL_UNSHARED_LIB_SUFFIX='90.lib'
 
 # Location of the top-level source directory from which Tcl was built.
 # This is the directory that contains a README file as well as
@@ -143,7 +139,7 @@ TCL_UNSHARED_LIB_SUFFIX='86t.lib'
 # different place than the directory containing the source files, this
 # points to the location of the sources, not the location where Tcl was
 # compiled.
-TCL_SRC_DIR='D:\a\1\b\externals\tcl-core-8.6.15.0\win\..'
+TCL_SRC_DIR='D:\a\1\b\externals\tcl-9.0.3.0\win\..'
 
 # List of standard directories in which to look for packages during
 # "package require" commands.  Contains the "prefix" directory plus also
@@ -154,27 +150,27 @@ TCL_PACKAGE_PATH='D:\a\1\a\amd64\lib'
 TCL_SUPPORTS_STUBS=1
 
 # The name of the Tcl stub library (.a):
-TCL_STUB_LIB_FILE='tclstub86.lib'
+TCL_STUB_LIB_FILE='tclstub.lib'
 
 # -l flag to pass to the linker to pick up the Tcl stub library
-TCL_STUB_LIB_FLAG='tclstub86.lib'
+TCL_STUB_LIB_FLAG='tclstub.lib'
 
 # String to pass to linker to pick up the Tcl stub library from its
 # build directory.
-TCL_BUILD_STUB_LIB_SPEC='-LD:\a\1\b\externals\tcl-core-8.6.15.0\win\Release_AMD64_VC13 tclstub86.lib'
+TCL_BUILD_STUB_LIB_SPEC='-LD:\a\1\b\externals\tcl-9.0.3.0\win\Release_AMD64_VC13 tclstub.lib'
 
 # String to pass to linker to pick up the Tcl stub library from its
 # installed directory.
-TCL_STUB_LIB_SPEC='-LD:\a\1\a\amd64\lib tclstub86.lib'
+TCL_STUB_LIB_SPEC='-LD:\a\1\a\amd64\lib tclstub.lib'
 
 # Path to the Tcl stub library in the build directory.
-TCL_BUILD_STUB_LIB_PATH='D:\a\1\b\externals\tcl-core-8.6.15.0\win\Release_AMD64_VC13\tclstub86.lib'
+TCL_BUILD_STUB_LIB_PATH='D:\a\1\b\externals\tcl-9.0.3.0\win\Release_AMD64_VC13\tclstub.lib'
 
 # Path to the Tcl stub library in the install directory.
-TCL_STUB_LIB_PATH='D:\a\1\a\amd64\lib\tclstub86.lib'
-
-# Flag, 1: we built Tcl with threads enabled, 0 we didn't
-TCL_THREADS=1
+TCL_STUB_LIB_PATH='D:\a\1\a\amd64\lib\tclstub.lib'
 
 # Name of the zlib library that extensions should use
 TCL_ZLIB_LIB_NAME='zdll.lib'
+
+# Name of the tommath library that extensions should use
+TCL_TOMMATH_LIB_NAME='tommath.lib'
